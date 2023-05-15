4a34525bcfa2bd3bcb0f3bfb29aa76827ec7cecb
m_sadatrazavi <m_sadatrazavi@ISC.CO.IR>
2023-05-15 11:15:13 +0330

    feat: ProductPriceRepositoryTest added

--- src/main/resources/sql/data-h2.sql
+++ src/main/resources/sql/data-h2.sql
@@ -1,4 +1,12 @@
+-- PRODUCT --------------------------------------------------------------
 INSERT INTO product (id, name) VALUES ('#1', 'Monitor');
 INSERT INTO product (id, name) VALUES ('#2', 'CPU');
 INSERT INTO product (id, name) VALUES ('#3', 'HDD');
 INSERT INTO product (id, name) VALUES ('#4', 'Main Board');
+-- PRODUCT_PRICE --------------------------------------------------------
+INSERT INTO product_price (id, product_id, price_date, price, active) VALUES ('#1', '#3', '2023-05-15 10:49', '1000', null);
+INSERT INTO product_price (id, product_id, price_date, price, active) VALUES ('#2', '#3', '2023-05-16 10:49', '1001', null);
+INSERT INTO product_price (id, product_id, price_date, price, active) VALUES ('#3', '#3', '2023-05-17 10:49', '1002', true);
+INSERT INTO product_price (id, product_id, price_date, price, active) VALUES ('#4', '#4', '2023-05-15 10:49', '2000', null);
+INSERT INTO product_price (id, product_id, price_date, price, active) VALUES ('#5', '#4', '2023-05-16 10:49', '3000', null);
+INSERT INTO product_price (id, product_id, price_date, price, active) VALUES ('#6', '#4', '2023-05-17 10:49', '4000', true);
---
+++ src/test/java/com/msrazavi/train/sw/price/ProductPriceRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.msrazavi.train.sw.price;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import java.util.Objects;
+
+/**
+ * @author <a href="mailto:mehdisr57@gmail.com">Mehdi.S.Razavi</a>
+ */
+@DataJpaTest
+@SqlGroup({
+        @Sql(value = "classpath:sql/data-h2.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+})
+class ProductPriceRepositoryTest {
+
+    private final String productId = "#3";
+
+    @Autowired
+    private ProductPriceRepository repository;
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Test
+    void findByProductIdTest() {
+        Assertions.assertEquals(3, repository.findByProductId(productId).size());
+    }
+
+    @Test
+    void deActivePricesOfProductTest() {
+        final long initDeActiveCount = repository.findByProductId(productId).stream()
+                .filter(productPrice -> Objects.isNull(productPrice.getActive()))
+                .count();
+        repository.deActivePricesOfProduct(productId);
+        entityManager.clear();
+        final long deActiveCount = repository.findByProductId(productId).stream()
+                .filter(productPrice -> Objects.isNull(productPrice.getActive()))
+                .count();
+        Assertions.assertNotEquals(initDeActiveCount, deActiveCount);
+    }
+}
